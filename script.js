2cb984ffe011ad2bb78ca096d6d4905ef40e2e38
joaopsalescarvalho <jp0489214@gmail.com>
2023-09-08 13:09:18 -0300

    adicionei arquivos

---
+++ script.js
@@ -0,0 +1,48 @@
+const screen1 = document.getElementById('screen1')
+const screen2 = document.getElementById('screen2')
+const altura = document.getElementById('altura')
+const peso = document.getElementById('peso')
+const res = document.getElementById('res')
+const botaoConfirmar= document.getElementById('confirmar')
+const botaoVoltar = document.getElementById('voltar')
+
+peso.addEventListener('input',validatepeso)
+altura.addEventListener('input',validatealtura)
+botaoConfirmar.addEventListener('click',nextScreen)
+botaoVoltar.addEventListener('click',prevScreen)
+
+function validatepeso(){
+    if(peso.value == 0){
+        peso.style.border='2px solid red'
+        peso.placeholder='Adicione um peso'
+    }else{
+        peso.style.border=''
+        peso.placeholder=''
+}
+}
+function validatealtura(){
+    if(altura.value == 0){
+        altura.style.border='2px solid red'
+        altura.placeholder='Adicione uma altura'
+    }else{
+        altura.style.border=''
+        altura.placeholder=''
+}
+}
+function nextScreen(){
+    if(altura.value == 0 || peso.value == 0){
+    validatepeso()
+    validatealtura()
+    }else{
+    let n1 =Number(peso.value);
+    let n2 =Number(altura.value);
+    const imc = (n1 / (n2 * n2)).toFixed(0);
+    screen1.style.display='none';
+    screen2.style.display='block';
+    res.innerHTML=`Seu IMC: ${imc}`;
+    }
+}
+function prevScreen(){
+    screen1.style.display='block';
+    screen2.style.display='none';
+}
